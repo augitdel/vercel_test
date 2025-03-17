13f0435d0a764d63032a7c54caa2947c5d7140ca
audlbeke <august.delbeke@ugent.be>
2025-03-17 22:50:16 +0100

    random number

---
+++ api/random_number.py
@@ -0,0 +1,21 @@
+import time
+import random
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('random_number.html')
+
+def generate_random_number():
+    while True:
+        time.sleep(5)
+        number = random.randint(1, 100)
+        socketio.emit('update_number', {'number': number})
+
+if __name__ == '__main__':
+    socketio.start_background_task(generate_random_number)
+    socketio.run(app, debug=True)
